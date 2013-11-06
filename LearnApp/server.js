e5d49ec353d44be892bdd12a9482e692cbb7458a
Zamir Musaev <zamir.ru@gmail.com>
2013-11-06 23:47:37 +0400

    Модули + имитация БД

--- LearnApp/server.js
+++ LearnApp/server.js
@@ -1,13 +1,26 @@
 ﻿// Node.js entry point
-var user = require('./user');
+var User = require('./user');
+var db = require('./db');
+db.connect();
 
-var vasya = new user.User('Вася');
-var petya = new User('Petya');
+function run() {
+    var vasya = new User('Вася');
+    var petya = new User('Petya');
 
-vasya.hello(petya);
+    vasya.hello(petya);
 
-require('readline')
-.createInterface(process.stdin, process.stdout)
-.question('Press [Enter] to exit...', function () {
-    process.exit();
-});
+    console.log(db.getPhrase('Run successful'));
+
+    require('readline')
+   .createInterface(process.stdin, process.stdout)
+   .question('Press [Enter] to exit...', function () {
+       process.exit();
+   });
+};
+
+if (module.parent) {
+    exports.run = run;
+} else {
+    run();
+}
+
--- LearnApp/user/index.js
+++ LearnApp/user/index.js
@@ -1,13 +1,12 @@
-﻿var phrases = require('./ru');
+﻿var db = require('../db');
+db.connect();
+
 function User(name) {
     this.name = name;
-}
+};
 
 User.prototype.hello = function (who) {
-    console.log(phrases.Hello + who.name);
-}
+    console.log(db.getPhrase('Hello') + ' ' + who.name);
+};
 
-console.log('User.js is required');
-
-exports.User = User;
-global.User = User;
+module.exports = User;
