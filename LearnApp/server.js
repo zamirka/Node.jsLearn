1d201fbdcaabbecfe53f6a05bd87c70f6d20848d
Zamir Musaev <zamir.ru@gmail.com>
2013-11-06 10:24:10 +0400

    Inint

---
+++ LearnApp/server.js
@@ -0,0 +1,13 @@
+﻿// Node.js entry point
+var user = require('./user');
+
+var vasya = new user.User('Вася');
+var petya = new User('Petya');
+
+vasya.hello(petya);
+
+require('readline')
+.createInterface(process.stdin, process.stdout)
+.question('Press [Enter] to exit...', function () {
+    process.exit();
+});
---
+++ LearnApp/user/index.js
@@ -0,0 +1,13 @@
+﻿var phrases = require('./ru');
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function (who) {
+    console.log(phrases.Hello + who.name);
+}
+
+console.log('User.js is required');
+
+exports.User = User;
+global.User = User;
